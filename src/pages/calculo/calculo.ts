4f157b3f87019fc5d3f331797c4b3c22b80798c9
Gaston Rivata <gaston.rivata@ayi-asociados.com>
2018-05-10 17:56:40 -0300

    Fotos y componentes

--- src/pages/calculo/calculo.ts
+++ src/pages/calculo/calculo.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 /**
  * Generated class for the CalculoPage page.
@@ -15,11 +16,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CalculoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  calculo: FormGroup;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public formBuilder: FormBuilder) {
+    this.calculo = this.crearForm();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CalculoPage');
+  }
+
+  private crearForm() {
+    return this.formBuilder.group({
+      'c11': [''],
+      'c12': [''],
+      'py1': [''],
+      'py2': [''],
+      'estudioMercado': [''],
+      'casoExito': [''],
+      'casoFallido': ['']
+    })
   }
 
 }
