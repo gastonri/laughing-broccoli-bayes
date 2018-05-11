ddeac2788dae8450e7f288d47457a16002a75221
Gaston Rivata <gaston.rivata@ayi-asociados.com>
2018-05-11 18:00:56 -0300

    Trabajo primer cuadro (A)

--- src/pages/calculo/calculo.ts
+++ src/pages/calculo/calculo.ts
@@ -16,28 +16,52 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CalculoPage {
 
-  calculo: FormGroup;
+  calculoForm: FormGroup;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public formBuilder: FormBuilder) {
-    this.calculo = this.crearForm();
+    this.calculoForm = this.crearForm();
   }
+
+  c11 = 0
+  c12 = 0
+  py1 = 0
+  estudioMercado = 0
+  casoExito = 0
+  casoFallido = 0
+
+  dx1 = 0;
+  py2 = 0;
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CalculoPage');
   }
 
-  private crearForm() {
+  crearForm() {
     return this.formBuilder.group({
       'c11': [''],
       'c12': [''],
       'py1': [''],
-      'py2': [''],
       'estudioMercado': [''],
       'casoExito': [''],
       'casoFallido': ['']
     })
+  }
+
+  calcular() {
+    this.c11 = parseFloat(this.calculoForm.value['c11']);
+    this.c12 = parseFloat(this.calculoForm.value['c12']);
+    this.py1 = parseFloat(this.calculoForm.value['py1']);
+    this.estudioMercado = parseFloat(this.calculoForm.value['estudioMercado']);
+    this.casoExito = parseFloat(this.calculoForm.value['casoExito']);
+    this.casoFallido = parseFloat(this.calculoForm.value['casoFallido']);
+
+    this.py2 = (1 - this.py1);
+    this.dx1 = 0;
+    console.log(this.py2);
+    this.dx1 = (this.c11 * (this.py1) + (this.c12) * this.py2);
+    console.log(this.dx1);
   }
 
 }
