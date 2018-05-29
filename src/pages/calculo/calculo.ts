beab54d3c41cec635c2765ad9b777c00a78b8c93
Gastón <gastonrivata@gmail.com>
2018-05-28 21:12:13 -0300

    Avanzado el cuadro B2 y B3.

--- src/pages/calculo/calculo.ts
+++ src/pages/calculo/calculo.ts
@@ -18,26 +18,43 @@ export class CalculoPage {
 
   calculoForm: FormGroup;
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public formBuilder: FormBuilder) {
+  constructor( public navCtrl: NavController,
+               public navParams: NavParams,
+               public formBuilder: FormBuilder ) {
     this.calculoForm = this.crearForm();
   }
 
-  c11 = 0
-  c12 = 0
-  py1 = 0
-  estudioMercado = 0
+  c11 = 0;
+  c12 = 0;
+  py1 = 0;
+  estudioMercado = 0;
 
   dx1 = 0;
   py2 = 0;
 
   //Variables cuadro B
-  t11 = 0
-  t12 = 0
-  t21 = 0
-  t22 = 0
+  b1t1y1 = 0;
+  b1t1y2 = 0;
+  b1t2y1 = 0;
+  b1t2y2 = 0;
 
+  //Variables cuadro B
+  b2t1y1 = 0;
+  b2t1y2 = 0;
+  b2t2y1 = 0;
+  b2t2y2 = 0;
+  b2y1 = 0;
+  b2y2 = 0;
+  b2t1 = 0;
+  b2t2 = 0;
+
+  //Variables cuadro B
+  b3t1y1 = 0;
+  b3t1y2 = 0;
+  b3t2y1 = 0;
+  b3t2y2 = 0;
+  b3t1 = 0;
+  b3t2 = 0;
 
 
   ionViewDidLoad() {
@@ -46,33 +63,54 @@ export class CalculoPage {
 
   crearForm() {
     return this.formBuilder.group({
-      'c11': [''],
-      'c12': [''],
-      'py1': [''],
-      'estudioMercado': [''],
-      't11': [''],
-      't22': ['']
-    })
+      'c11': [ '' ],
+      'c12': [ '' ],
+      'py1': [ '' ],
+      'estudioMercado': [ '' ],
+      't11': [ '' ],
+      't22': [ '' ]
+    });
   }
 
   calcular() {
-    this.c11 = parseFloat(this.calculoForm.value['c11']);
-    this.c12 = parseFloat(this.calculoForm.value['c12']);
-    this.py1 = parseFloat(this.calculoForm.value['py1']);
-    this.estudioMercado = parseFloat(this.calculoForm.value['estudioMercado']);
-    this.t11 = parseFloat(this.calculoForm.value['t11']);
-    this.t22 = parseFloat(this.calculoForm.value['t22']);
+    this.c11 = parseFloat(this.calculoForm.value[ 'c11' ]);
+    this.c12 = parseFloat(this.calculoForm.value[ 'c12' ]);
+    this.py1 = parseFloat(this.calculoForm.value[ 'py1' ]);
+    this.estudioMercado = parseFloat(this.calculoForm.value[ 'estudioMercado' ]);
+    this.b1t1y1 = parseFloat(this.calculoForm.value[ 't11' ]);
+    this.b1t2y2 = parseFloat(this.calculoForm.value[ 't22' ]);
 
-    //Cuadro A
-    this.py2 = (1 - this.py1);
+    // Cuadro A
+    this.py2 = parseFloat(Number(1 - this.py1).toFixed(2));
     this.dx1 = 0;
-    this.dx1 = (this.c11 * (this.py1) + (this.c12) * this.py2);
+    this.dx1 = parseFloat(Number(this.c11 * (this.py1) + (this.c12) * this.py2).toFixed(2));
 
-    //Cuadro B
-    this.t12 = 1 - this.t11;
-    this.t21 = 1 - this.t22;
-    this.t12 = parseFloat(Number(this.t12).toFixed(2));
-    this.t21 = parseFloat(Number(this.t21).toFixed(2));
+    // Cuadro B1
+    this.b1t1y2 = 1 - this.b1t2y2;
+
+    this.b1t2y1 = 1 - this.b1t1y1;
+
+    this.b1t1y2 = parseFloat(Number(this.b1t1y2).toFixed(2));
+    this.b1t2y1 = parseFloat(Number(this.b1t2y1).toFixed(2));
+
+    // Cuadro B2
+    this.b2t1y1 = parseFloat(Number(this.b1t1y1 * this.py1).toFixed(3));
+    this.b2t1y2 = parseFloat(Number(this.b1t1y2 * this.py2).toFixed(3));
+
+    this.b2t2y1 = parseFloat(Number(this.b1t2y1 * this.py1).toFixed(3));
+    this.b2t2y2 = parseFloat(Number(this.b1t2y2 * this.py2).toFixed(3));
+
+    this.b2y1 = parseFloat(Number(this.b2t1y1 + this.b2t2y1).toFixed(3));
+    this.b2y2 = parseFloat(Number(this.b2t1y2 + this.b2t2y2).toFixed(3));
+    this.b2t1 = parseFloat(Number(this.b2t1y1 + this.b2t1y2).toFixed(3));
+    this.b2t2 = parseFloat(Number(this.b2t2y1 + this.b2t2y2).toFixed(3));
+
+    // Cuadro 3
+    this.b3t1y1 = parseFloat(Number(this.b2t1y1 / this.b2t1).toFixed(3));
+    this.b3t1y2 = parseFloat(Number(1 - this.b3t1y1).toFixed(3));
+    this.b3t2y1 = parseFloat(Number(this.b2t2y1 / this.b2t2).toFixed(3));
+    this.b3t2y2 = parseFloat(Number(1 - this.b3t2y1).toFixed(3));
+    this.b3t1 = parseFloat(Number(this.b3t1y1 + this.b3t1y2).toFixed(2));
+    this.b3t2 = parseFloat(Number(this.b3t2y1 + this.b3t2y2).toFixed(2));
   }
-
 }
